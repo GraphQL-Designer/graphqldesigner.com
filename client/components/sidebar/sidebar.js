c5cc4d9e908cafddae49876bcbfa394ecfd2fae8
James Sieu <jamessieu@gmail.com>
2018-09-11 22:53:31 -0700

    initial commit before merging in redux changes

---
+++ client/components/sidebar/sidebar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
+
+import './sidebar.css';
+
+export default class DrawerOpenRightExample extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false, value: 1};
+
+    this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle () {this.setState({open: !this.state.open})};
+
+  render() {
+    return (
+      // <div>
+        <Drawer width={300} openSecondary={true} open={true} >
+          <container>
+          <AppBar title="New Table" />
+            <newtable>
+              <h4>New Table</h4>
+              <TextField hintText='name'/>
+              <RaisedButton label="Create" primary={true} />
+            </newtable>
+              <Divider />
+            <options>
+              <h4>Options</h4>
+            </options>
+          </container>
+        </Drawer>
+      // </div>
+    );
+  }
+}
