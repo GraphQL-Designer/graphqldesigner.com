cdac9134e89515d610f1881b980261cb123ac9a8
Patrick Slagle <slaglebagel17@gmail.com>
2018-09-13 11:21:18 -0700

    updated table to include previous code

--- client/components/schema/table.js
+++ client/components/schema/table.js
@@ -20,6 +20,46 @@ const mapDispatchToProps = dispatch => ({
           <div>Table Field<button>x</button></div>
           <button>Add Field</button>
         </div>  
+          //     <div>
+          //     <table>
+          //       <tbody>
+          //         <tr>
+          //           <th colSpan={2}>    
+          //             <FormControl className="tableName" 
+          //               type="text" 
+          //               value=''
+          //               placeholder="Table Name"
+          //               onChange={(event) => {
+          //                 props.updateTableName(event.target.value);
+          //               }} 
+          //             />     
+          //           </th>
+          //         </tr>
+          //         <tr>
+          //         <td><FormControl className='fieldInput' 
+          //         type="text" 
+          //         placeholder="property" 
+          //         value=''
+          //         onChange={(event) => {
+          //           props.updateRowProp(event.target.value);
+          //         }}
+          //         />
+          //         </td>    
+          //         </tr>
+          //             <tr>
+          //               <td className='addFieldWrap' colSpan={2}>
+          //                 <button type="addField" onClick={() => addField(this.props.addField)}> ADD FIELD </button>
+          //               </td>
+          //             </tr>                               
+          //     </tbody>
+          //   </table>
+          //   <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip"> Delete table</Tooltip>}>
+          //     <div className="deleteTableButton" onClick={() => deleteTable(this.props.deleteTable)}>x</div>
+          //   </OverlayTrigger> 
+          //   <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip"> Delete field</Tooltip>}>
+          //     <div className='deletefield' onClick={() =>deleteField(this.props.deleteField)}>x</div>
+          //   </OverlayTrigger>
+          // </div> 
       )
     }
 }
