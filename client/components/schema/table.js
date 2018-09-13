e729e76984fb427b2e96741690e0df831abfa713
Tsion Adefres <tsiona@Tsions-MacBook-Air-3.local>
2018-09-12 22:41:44 -0700

    components no longer breaking

--- client/components/schema/table.js
+++ client/components/schema/table.js
@@ -5,7 +5,7 @@ import { connect} from 'react-redux';
 
 const mapDispatchToProps = dispatch => ({
   deleteTable: tableIndex => dispatch(actions.deleteTable(tableIndex)),
-  addField: fieldName => dispatch(actions.addField(fieldName)),
+  // addField: fieldName => dispatch(actions.addField(fieldName)),
   deleteField: fieldName => dispatch(actions.deleteField(fieldName)),
 })
 
@@ -20,32 +20,28 @@ const mapDispatchToProps = dispatch => ({
         <table>
           <tbody>
             <tr>
-              <th colSpan={2}>   
+              <th colSpan={2}>    
                 <FormControl className="tableName" 
                   type="text" 
-                  value= '' 
+                  value=''
                   placeholder="Table Name"
                   onChange={(event) => {
                     props.updateTableName(event.target.value);
                   }} 
-                />
-
-                   
+                />     
               </th>
             </tr>
             <tr>
             <td><FormControl className='fieldInput' 
             type="text" 
             placeholder="property" 
-            value= '' 
+            value=''
             onChange={(event) => {
-              // props.updateRowProp(event.target.value);
+              props.updateRowProp(event.target.value);
             }}
             />
-            </td>
+            </td>    
             </tr>
-              
-                       
                 <tr>
                   <td className='addFieldWrap' colSpan={2}>
                     <button type="addField" onClick={() => addField(this.props.addField)}> ADD FIELD </button>
@@ -54,10 +50,10 @@ const mapDispatchToProps = dispatch => ({
         </tbody>
       </table>
       <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip"> Delete table</Tooltip>}>
-                    <div className="deleteTableButton" onClick={() => deleteTable(this.props.deleteTable)}>x</div>
-                  </OverlayTrigger>
+        <div className="deleteTableButton" onClick={() => deleteTable(this.props.deleteTable)}>x</div>
+      </OverlayTrigger> 
       <OverlayTrigger placement="right" overlay={<Tooltip id="tooltip"> Delete field</Tooltip>}>
-      <div className='deletefield' onClick={() =>deleteField(this.props.deleteField)}>x</div>
+        <div className='deletefield' onClick={() =>deleteField(this.props.deleteField)}>x</div>
       </OverlayTrigger>
     </div>  
     )
