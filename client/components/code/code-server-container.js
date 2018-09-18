d3e79fd019a213ac64c784281026411687c97460
James Sieu <jamessieu@gmail.com>
2018-09-17 22:15:45 -0700

    added framework for code section

---
+++ client/components/code/code-server-container.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './code.css';
+
+const CodeServerContainer = props => {
+  return (
+    <div className='code-server-container'>
+      <h4>Server</h4>
+    </div>
+  )
+};
+
+export default CodeServerContainer
