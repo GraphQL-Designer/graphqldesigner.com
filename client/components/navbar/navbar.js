c7e60bd466204411329270328382c7078b9868e2
Greg <gregshamalta@yahoo.com>
2018-09-19 12:13:39 -0700

    broken download

--- client/components/navbar/navbar.js
+++ client/components/navbar/navbar.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 //const topAppBar = new MDCTopAppBar(topAppBarElement);
 
 const mapStateToProps = store => ({
-  table: store.data.table,  
+  tables: store.data.tables,
 });
 
 
@@ -20,20 +20,42 @@ class MainNav extends React.Component {
   constructor(props) {
     super(props);
     //this.handleSave= this.handleSave.bind(this)
-    //this.handleExport = this.handleExportbind(this)
+    this.handleExport = this.handleExport.bind(this)
   }
 
   handleExport(event){
-    event.preventDefault();
-    this.props.exportTable(this.props.table);
-    // this.props.handleExport({
-    //   tableIndex: this.props.tableIndex,
-    //   fieldIndex: event.target.id,
-    //   submitUpdate: false
-    // })
-  }
-  
+    const data = Object.assign({}, {data: this.props.tables}, {
+      database: 'MongoDB'
+    })
+    fetch('http://localhost:4100/write-files', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+     })
 
+     //.then(res => console.log(res))
+    //  .then(res => new Response(res.body))
+    //  .then(response => {
+    //    console.log('res', response)
+    //    console.log('res.body==>>>', response.body)
+    //    return response.blob()
+    //   })
+    //  .then(blob => {
+    //    console.log('blob', blob)
+    //    return URL.createObjectURL(blob)
+    //  })
+    //  .then(file => {
+    //     var element = document.createElement("a");
+    //     element.href = file;
+    //     element.download = "graphql.txt";
+    //     console.log('file', file)
+    //     element.click();
+    //  })
+
+     .catch((err) => console.log(err))
+    }
   
   render() {
     return (
