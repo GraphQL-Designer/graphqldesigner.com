4974fb24b42ac836ec8180582efe13addffc7ce1
Greg <gregshamalta@yahoo.com>
2018-09-19 14:36:46 -0700

    remove build files

--- server/build-files1537392480477/server/db-Model/.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const Schema = new Schema({
-	
-});
-
-module.exports = mongoose.model("", Schema);
--- server/build-files1537392640571/server/db-Model/ef.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const efSchema = new Schema({
-	
-});
-
-module.exports = mongoose.model("Ef", efSchema);
