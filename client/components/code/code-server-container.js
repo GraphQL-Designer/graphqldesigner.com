da27508fb6f75fbbf0102f39e2e289ef396fbe4f
Tsion Adefres <tadefres@gmail.com>
2018-09-29 14:14:38 -0700

    working on server code

--- client/components/code/code-server-container.js
+++ client/components/code/code-server-container.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
 import './code.css';
 
-const CodeServerContainer = props => {
+const CodeServerContainer = (props) => {
+  const enter = `
+  `;
+  const tab = ` `;
+    let serverCode = `const graphql = require('graphql');${enter}`;
+function parseGraphqlMongoServer(data) {
+    for (const prop in data) {
+      serverCode += buildDbModelRequirePaths(data[prop]);
+    }
+
+    serverCode += `
+  const { 
+      GraphQLObjectType,
+      GraphQLSchema,
+      GraphQLID,
+      GraphQLString, 
+      GraphQLInt, 
+      GraphQLList,
+      GraphQLNonNull
+  } = graphql;
+  ${enter}`;
+
+
+    return serverCode += `${enter}${tab}})${enter}});${enter}${enter}`;
+  }
   return (
-    <div className='code-container-middle'>
-      <h4>Server</h4>
+    <div className="code-container-middle">
+      <pre>
+        {serverCode}
+      </pre>
     </div>
-  )
+  );
 };
 
-export default CodeServerContainer;
+export default connect(null, null)(CodeServerContainer);
