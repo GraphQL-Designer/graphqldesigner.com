5e0e0c5fe3908f84ef50be9356739481ec750f7f
Patrick Slagle <slaglebagel17@gmail.com>
2018-09-29 17:07:10 -0700

    removed unused code from queryReducers

--- client/reducers/queryReducers.js
+++ client/reducers/queryReducers.js
@@ -1,31 +1,7 @@
 import * as types from '../actions/action-types';
 
 const initialState = {
-  projectReset: true,
-  tableIndex: 0,
-  tables: {},
-  selectedQuery: {
-    fields: {},
-    tableID: -1,
-  },
-  selectedField: {
-    name: '',
-    type: 'String',
-    primaryKey: false,
-    unique: false,
-    defaultValue: '',
-    required: false,
-    multipleValues: false,
-    relationSelected: false,
-    relation: {
-      tableIndex: -1,
-      fieldIndex: -1,
-      refType: '',
-    },
-    refBy: new Set(),
-    tableNum: -1,
-    fieldNum: -1,
-  },
+
 };
 
 const queryReducers = (state = initialState, action) => {
