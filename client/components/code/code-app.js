9a8e79e97357c549f57d5c2bc0f980dd557f8261
Patrick Slagle <slaglebagel17@gmail.com>
2018-10-02 00:30:43 -0700

    finished making scripts for SQL

--- client/components/code/code-app.js
+++ client/components/code/code-app.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 
 // components
-import CodeDBSchemaContainer from './code-dbschema-container.js';
-import CodeSqlDBSchemaContainer from './code-sqlschema-container.js';
+import CodeDBMongoContainer from './code-db-mongo-container.js';
+import CodeDBSQLContainer from './code-db-sql-container.js';
+import CodeDBSequelizeContainer from './code-db-sequelize-container.js';
 import CodeClientContainer from './code-client-container.js';
 import CodeServerContainer from './code-server-container.js';
 
@@ -16,8 +17,9 @@ class CodeApp extends Component {
     return (
       <div className='code-app'>
         <div className='wallpaper'></div>
-        <CodeDBSchemaContainer/>
-        {/*<CodeSqlDBSchemaContainer/>*/}
+        {/* <CodeDBMongoContainer/> */}
+        {/*<CodeSqlDBSequelizeContainer/>*/}
+        <CodeDBSQLContainer/>
         <CodeServerContainer/>
         <CodeClientContainer/>
       </div>
