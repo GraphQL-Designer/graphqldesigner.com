c4c792ea3212c90bdceaa967acd42838c8a7f53a
James Sieu <jamessieu@gmail.com>
2018-10-09 13:51:04 -0700

    create_readme.js and express_server.js temp

--- server/create_file_func/create_readme.js
+++ server/create_file_func/create_readme.js
@@ -1,31 +1,36 @@
 function createPackageJson(data) {
   const query = `
   -create a project folder "my-app"
+    -create a .env file and add MONGO_URI="your Mongo database url" for MongoDB or
+                                SQL_URI="your SQL database url"
     -unzip exported project from GraphQL Designer and move server folder in project folder "my-app"
     -create a package.json file  in "my-app" folder with npm -init
-  -Add the required dependencies below to your package.json file
+      -Add the required dependencies below to your package.json file
 
-  //Required Dependencies
-    "apollo-boost": "^0.1.15",
-    "express": "^4.16.3",
-    "express-graphql": "^0.6.12",
-    "graphql": "^14.0.0",
-    "react-apollo": "^2.1.11"
+        //Required Dependencies
+        "dependencies": {
+          "apollo-boost": "^0.1.15",
+          "dotenv": "^6.0.0",
+          "express": "^4.16.3",
+          "express-graphql": "^0.6.12",
+          "graphql": "^14.0.0",
+          "react-apollo": "^2.1.11"
+        }
 
-    -server/index.js contains the express server information.  Default port is 4000, but this can be changed to your liking
-    -for MongoDB, please enter your database path in server/index.js
+          -add the following to the dependencies depending on database selected 
+          //MongoDB Dependencies
+            "mongoose": "^5.2.9"
+        
+          //MySQL Dependencies
+
+    -in server/index.js contains the express server information.  Default port is 4000, but this can be changed to your liking
   -server/graphql-schema/index.js contains schemas based on database created using GraphQL Designer
   -GraphQL mutations and queries (in client folder) and schemas(in server folder) are created, ready to be put into your code base
-  -add the following depending on database selected previously
-  //MongoDB Dependencies
-    "mongoose": "^5.2.9"
-
-  //MySQL Dependencies
-
+  
     -start server by running "node server/index.js"
     -to test the backend go to localhost:4000/graphiql to use tgit stathe query interface to test queries and create mutations made using the application
   
-  -use create react app in "my-app" folder and create a new folder called client by entering "init react-app client"
+  -(Optional)use create react app in "my-app" folder and create a new folder called client by entering "init react-app client"
     -move graphql folder and its content extracted from GraphQL Designer to client/src
   
   the folder "my-app" should have the following structure
@@ -55,13 +60,14 @@ function createPackageJson(data) {
   │               └── index.js
   ├── server
   │   ├── index.js
-  │   ├── db-model
+  │   ├── db
   │   │   └── all schema models
   │   └── graphql-schema
   │       └── index.js
+  ├── .env
   └── package.json  
 
-  `
+  `;
   return query;
 }
 
--- server/create_file_func/express_server.js
+++ server/create_file_func/express_server.js
@@ -1,5 +1,7 @@
 function buildExpressServer(database) {
-let query = `const express = require('express');
+let query = `
+require('dotenv').config();
+const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./graphql-schema');
 const path = require('path');
@@ -10,7 +12,7 @@ if (database === 'MongoDB') {
   query += `
 const mongoose = require('mongoose');
 
-mongoose.connect('Your Database Here!');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () => console.log('connected to database'));
 `;
 }
 
