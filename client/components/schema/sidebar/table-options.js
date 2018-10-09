2d9627892708bc9c8b999c4ffa4b577b31dc42a9
Patrick Slagle <slaglebagel17@gmail.com>
2018-10-09 16:58:29 -0700

    Don't wan't to disable ref type option yet

--- client/components/schema/sidebar/table-options.js
+++ client/components/schema/sidebar/table-options.js
@@ -326,8 +326,8 @@ class TableOptions extends React.Component {
                   </DropDownMenu>
                 </div>
 
-                {this.props.database === 'MongoDB' &&
-                  (<div className='relation-options'>
+               // {this.props.database === 'MongoDB' && (
+                  <div className='relation-options'>
                     <p>RefType:</p>
                     <DropDownMenu
                       value={this.props.selectedField.relation.refType}
@@ -339,8 +339,8 @@ class TableOptions extends React.Component {
                       <MenuItem value='many to one' primaryText="many to one" />
                       {/* <MenuItem value='many to many' primaryText="many to many" /> */}
                     </DropDownMenu>
-                  </div>)
-                }
+                  </div>
+                //)}
               </span>)}
               <RaisedButton
                 secondary={true}
