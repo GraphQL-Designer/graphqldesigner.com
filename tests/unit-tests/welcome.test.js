ba5c8efd626fe19141223d56bd85dcd5eda891c6
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-04 21:13:50 -0800

    added tests for welcome

--- tests/unit-tests/welcome.test.js
+++ tests/unit-tests/welcome.test.js
@@ -1,27 +1,27 @@
 import reducers from '../../client/reducers/schemaReducers';
 
 test('User can select a MongoDB database', () => {
-  let state = reducers({}, {type:'CHOOSE_DATABASE',payload:'MongoDB'});
+  const state = reducers({}, {type:'CHOOSE_DATABASE',payload:'MongoDB'});
   expect(state.database).toEqual('MongoDB');
 });
 
 test('User can select a MySQL database', () => {
-  let state = reducers({}, {type:'CHOOSE_DATABASE',payload:'MySQL'});
+  const state = reducers({}, {type:'CHOOSE_DATABASE',payload:'MySQL'});
   expect(state.database).toEqual('MySQL');
 });
 
 test('User can select a PostgreSQL database', () => {
-  let state = reducers({}, {type:'CHOOSE_DATABASE',payload:'PostgreSQL'});
+  const state = reducers({}, {type:'CHOOSE_DATABASE',payload:'PostgreSQL'});
   expect(state.database).toEqual('PostgreSQL');
 });
 
 test('User can select a PostgreSQL database utilizing Join Monster', () => {
-  let state = reducers({}, {type:'CHOOSE_DATABASE',payload:'PostgreSQL+JoinMonster'});
+  const state = reducers({}, {type:'CHOOSE_DATABASE',payload:'PostgreSQL+JoinMonster'});
   expect(state.database).toEqual('PostgreSQL+JoinMonster');
 });
 
 test('User can select a MySQL database utilizing Join Monster', () => {
-  let state = reducers({}, {type:'CHOOSE_DATABASE',payload:'MySQL+JoinMonster'});
+  const state = reducers({}, {type:'CHOOSE_DATABASE',payload:'MySQL+JoinMonster'});
   expect(state.database).toEqual('MySQL+JoinMonster');
 });
 
