2ab8523888996e049136f4fd40a8cefff2b570e7
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-17 22:12:05 -0800

    making code component more dynamic and efficient

--- client/components/code/code-app.js
+++ client/components/code/code-app.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 // components
-import CodeDBMongoContainer from './database-code/code-db-mongo-container.js';
-import CodeDBSQLContainer from './database-code/code-db-mysql-container.js';
-import CodeDBPostgresSchemaContainer from './database-code/code-db-postgres-container.js';
 import CodeClientContainer from './client-code/code-client-container.js';
 import CodeServerContainer from './server-code/code-server-container.js';
+import DbCodeContainer from './database-code/code-db-container.js'
 
 const mapStateToProps = store => ({
   database: store.schema.database,
@@ -18,14 +16,10 @@ class CodeApp extends Component {
   }
 
   render() {
-    let databaseContainer = <CodeDBMongoContainer/>
-    if (this.props.database === 'MySQL') databaseContainer = <CodeDBSQLContainer/>
-    if (this.props.database === 'PostgreSQL') databaseContainer = <CodeDBPostgresSchemaContainer/>
-  
     return (
       <div className='code-app'>
         <div className='wallpaper-code'></div>
-        {databaseContainer}
+        <DbCodeContainer/>
         <CodeServerContainer/>
         <CodeClientContainer/>
       </div>
