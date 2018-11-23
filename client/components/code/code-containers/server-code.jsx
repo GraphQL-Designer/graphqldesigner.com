4ade186dc4b5f8ff5f30a586b275bfe4a24b9310
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-22 23:22:03 -0800

    linted server-code file

--- client/components/code/code-containers/server-code.jsx
+++ client/components/code/code-containers/server-code.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import buildServerCode from '../../../../utl/create_file_func/graphql_server.js' 
+import buildServerCode from '../../../../utl/create_file_func/graphql_server';
 
 // styling
 import '../code.css';
@@ -10,12 +10,12 @@ const mapStateToProps = store => ({
   tables: store.schema.tables,
 });
 
-const CodeServerContainer = (props) => {
-  const serverCode = buildServerCode(props.tables, props.database)
+const CodeServerContainer = ({ tables, database }) => {
+  const serverCode = buildServerCode(tables, database);
   return (
     <div id="code-container-server">
-      <h4 className='codeHeader'>GraphQl Types, Root Queries, and Mutations</h4>
-      <hr/>
+      <h4 className="codeHeader">GraphQl Types, Root Queries, and Mutations</h4>
+      <hr />
       <pre>
         {serverCode}
       </pre>
