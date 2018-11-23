fa5658296278ef3bcfaa23a7bac10546b5338134
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-22 23:21:07 -0800

    linting

--- client/components/code/code-containers/db-code.jsx
+++ client/components/code/code-containers/db-code.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import buildMySQLScripts from '../../../../utl/create_file_func/mysql_scripts.js'
-import buildMongoSchema from '../../../../utl/create_file_func/mongo_schema.js'
-import buildPostgreSQLScripts from '../../../../utl/create_file_func/postgresql_scripts.js'
+import buildMySQLScripts from '../../../../utl/create_file_func/mysql_scripts';
+import buildMongoSchema from '../../../../utl/create_file_func/mongo_schema';
+import buildPostgreSQLScripts from '../../../../utl/create_file_func/postgresql_scripts';
 
 // Styling
 import '../code.css';
@@ -13,45 +13,44 @@ const mapStateToProps = store => ({
   database: store.schema.database,
 });
 
-const CodeDBSQLContainer = (props) => {
+const CodeDBSQLContainer = ({ database, tables }) => {
   let databaseCode = '';
   let header = '';
   const enter = `
 `
 
-  switch (props.database) {
+  switch (database) {
     case 'MongoDB':
       header = 'MongoDB Schemas';
-      databaseCode = [];
-      for (const tableId in props.tables) {
-        databaseCode.push(
-          <pre key={'mongoSchema' + tableId}>
-            {buildMongoSchema(props.tables[tableId])}
-            {enter}
-            {enter}
-            <hr/>
-          </pre>
-        )
-      }
-      break; 
+      databaseCode = Object.keys(tables).map(tableId => (
+        <pre key={`mongoSchema${tableId}`}>
+          {buildMongoSchema(tables[tableId])}
+          {enter}
+          {enter}
+          <hr />
+        </pre>
+      ));
+      break;
     case 'PostgreSQL':
-      databaseCode = buildPostgreSQLScripts(props.tables);
-      header = 'PostgreSQL Create Scripts'
+      databaseCode = buildPostgreSQLScripts(tables);
+      header = 'PostgreSQL Create Scripts';
       break; 
     case 'MySQL':
-      databaseCode = buildMySQLScripts(props.tables);
-      header = 'MySQL Create Scripts'
-      break; 
+      databaseCode = buildMySQLScripts(tables);
+      header = 'MySQL Create Scripts';
+      break;
+    default:
+      break;
   }
 
   return (
     <div id="code-container-database">
-      <h4 className='codeHeader'>{header}</h4>
-      <hr/>
+      <h4 className="codeHeader">{header}</h4>
+      <hr />
       <pre>
         {databaseCode}
       </pre>
-      <pre id='column-filler-for-scroll'></pre>
+      <pre id="column-filler-for-scroll" />
     </div>
   );
 };
