f70dc58bde352bc45afed2801c7059afb4109f5b
Patrick Slagle <slaglebagel17@gmail.com>
2018-11-25 16:10:51 -0800

    turned schema-app into a stateless component

--- client/components/schema/schema-app.jsx
+++ client/components/schema/schema-app.jsx
@@ -17,69 +17,56 @@ const mapStateToProps = store => ({
   selectedField: store.schema.selectedField,
 });
 
-class SchemaApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sidebar: true,
-    };
-  }
-
-  render() {
-    // Dynamically renders each table based on the number of tables.
-    const tableComponents = [];
-    const keyNum = 100; // React requires a key to avoid errors.
-    Object.keys(this.props.tables).forEach(tableIndex => (
-      tableComponents.push(
-        <CSSTransition
+const SchemaApp = props => {
+  // Dynamically renders each table based on the number of tables.
+  const tableComponents = [];
+  Object.keys(props.tables).forEach(tableIndex => (
+    tableComponents.push(
+      <CSSTransition
+        key={tableIndex}
+        timeout={100}
+        classNames="fadeScale"
+      >
+        <Table
           key={tableIndex}
-          timeout={100}
-          classNames="fadeScale"
-        >
-          <Table
-            key={tableIndex}
-            tableData={this.props.tables[tableIndex]}
-            tableIndex={tableIndex}
-            fieldCount={this.props.tables[tableIndex].fieldCount}
-          />
-        </CSSTransition>,
-      )
-    ));
-
-    let sidebar = '';
-    return (
-      <div className="schema-app-container">
-        <CSSTransition
-          in={this.state.sidebar}
-          timeout={200}
-          classNames="fade"
-        >
-          <div id="sidebar-container">
-            <CSSTransition
-              in={this.props.selectedField.tableNum < 0}
-              key="table"
-              timeout={200}
-              classNames="fade"
-            >
-              <CreateTable/>
-            </CSSTransition>
-
-            <CSSTransition
-              in={this.props.selectedField.tableNum >= 0}
-              key="fields"
-              timeout={200}
-              classNames="fade"
-            >
-              <TableOptions/>
-            </CSSTransition>
-          </div>
-        </CSSTransition>
-        <TransitionGroup className="table-components-container" id="wallpaper-schema">
-          {tableComponents}
-        </TransitionGroup>
-      </div>
-    );
-  }
+          tableData={props.tables[tableIndex]}
+          tableIndex={tableIndex}
+          fieldCount={props.tables[tableIndex].fieldCount}
+        />
+      </CSSTransition>,
+    )
+  ));
+  return (
+    <div className="schema-app-container">
+      <CSSTransition
+        in={true}
+        timeout={200}
+        classNames="fade"
+      >
+        <div id="sidebar-container">
+          <CSSTransition
+            in={props.selectedField.tableNum < 0}
+            key="table"
+            timeout={200}
+            classNames="fade"
+          >
+            <CreateTable/>
+          </CSSTransition>
+          <CSSTransition
+            in={props.selectedField.tableNum >= 0}
+            key="fields"
+            timeout={200}
+            classNames="fade"
+          >
+            <TableOptions/>
+          </CSSTransition>
+        </div>
+      </CSSTransition>
+      <TransitionGroup className="table-components-container" id="wallpaper-schema">
+        {tableComponents}
+      </TransitionGroup>
+    </div>
+  );
 }
 
 export default connect(mapStateToProps, null)(SchemaApp);
