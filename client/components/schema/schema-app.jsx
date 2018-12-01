0746e3479547b8e3e79d664cdfedf346b0725e7a
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-01 15:38:59 -0800

    removed unnecessary import of react component, since using stateless component

--- client/components/schema/schema-app.jsx
+++ client/components/schema/schema-app.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
